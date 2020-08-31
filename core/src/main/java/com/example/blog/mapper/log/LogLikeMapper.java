3f44f0e01219d1a91f89df7bca29ec87dc7ed277
shishunyuan <shishunyuan@iboxpay.com>
2020-08-31 14:58:33 +0800

    优化配置文件

--- core/src/main/java/com/example/blog/mapper/log/LogLikeMapper.java
+++ core/src/main/java/com/example/blog/mapper/log/LogLikeMapper.java
@@ -2,6 +2,8 @@ package com.example.blog.mapper.log;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.example.blog.entity.log.LogLike;
+import org.apache.ibatis.annotations.Mapper;
 
+@Mapper
 public interface LogLikeMapper extends BaseMapper<LogLike> {
 }
